b6d0e6213aec2f3b5831d50a3d10e7b2f96d5618
karl <2557075675@qq.com>
2024-03-06 16:07:41 +0800

    feat: add the initial UI

--- main.ts
+++ main.ts
@@ -1,58 +1,65 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
-import { UniverService } from './UniverService';
-// Remember to rename these classes and interfaces!
+import {
+	App,
+	Editor,
+	MarkdownView,
+	Modal,
+	Notice,
+	Plugin,
+	PluginSettingTab,
+	Setting,
+} from "obsidian";
 
 interface MyPluginSettings {
 	mySetting: string;
 }
 
 const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
- 
+	mySetting: "default",
+};
+
 export default class MyPlugin extends Plugin {
 	settings: MyPluginSettings;
 
 	async onload() {
-		console.log('univer plugin has beginning')
+		console.log("univer plugin has beginning");
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('cable', 'Univer Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('Hello, welcome to Univer!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
+		const ribbonIconEl = this.addRibbonIcon(
+			"cable",
+			"Univer",
+			(evt: MouseEvent) => {
+				new ChooseTypeModal(this.app).open();
 			}
+		);
+		ribbonIconEl.addClass("my-plugin-ribbon-class");
+
+		const statusBarItemEl = this.addStatusBarItem();
+		statusBarItemEl.setText("Hello Univer");
+
+		this.addCommand({
+			id: "open-choose-type-modal",
+			name: "create the product",
+			callback: () => {
+				new ChooseTypeModal(this.app).open();
+			},
 		});
 		// This adds an editor command that can perform some operation on the current editor instance
 		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
+			id: "sample-editor-command",
+			name: "Sample editor command",
 			editorCallback: (editor: Editor, view: MarkdownView) => {
 				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
+				editor.replaceSelection("Sample Editor Command");
+			},
 		});
 		// This adds a complex command that can check whether the current state of the app allows execution of the command
 		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
+			id: "open-sample-modal-complex",
+			name: "Open sample modal (complex)",
 			checkCallback: (checking: boolean) => {
 				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
+				const markdownView =
+					this.app.workspace.getActiveViewOfType(MarkdownView);
 				if (markdownView) {
 					// If checking is true, we're simply "checking" if the command can be run.
 					// If checking is false, then we want to actually perform the operation.
@@ -63,7 +70,7 @@ export default class MyPlugin extends Plugin {
 					// This command will only show up in Command Palette when the check function returns true
 					return true;
 				}
-			}
+			},
 		});
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
@@ -71,27 +78,29 @@ export default class MyPlugin extends Plugin {
 
 		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
 		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
+		this.registerDomEvent(document, "click", (evt: MouseEvent) => {
+			console.log("click", evt);
 		});
 
 		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		this.registerInterval(
+			window.setInterval(() => console.log("setInterval"), 5 * 60 * 1000)
+		);
 	}
 
 	onunload() {
-		console.log('univer plugin has ended')
+		console.log("univer plugin has ended");
 	}
 
 	async loadSettings() {
 		// this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
 
-		const data = await this.loadData()
-		if(data) {
-			this.settings = Object.assign({}, DEFAULT_SETTINGS, data)
+		const data = await this.loadData();
+		if (data) {
+			this.settings = Object.assign({}, DEFAULT_SETTINGS, data);
 		} else {
-			this.settings = DEFAULT_SETTINGS
-			await this.saveData(this.settings)
+			this.settings = DEFAULT_SETTINGS;
+			await this.saveData(this.settings);
 		}
 	}
 
@@ -106,12 +115,12 @@ class SampleModal extends Modal {
 	}
 
 	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
+		const { contentEl } = this;
+		contentEl.setText("Woah!");
 	}
 
 	onClose() {
-		const {contentEl} = this;
+		const { contentEl } = this;
 		contentEl.empty();
 	}
 }
@@ -125,19 +134,67 @@ class SampleSettingTab extends PluginSettingTab {
 	}
 
 	display(): void {
-		const {containerEl} = this;
+		const { containerEl } = this;
 
 		containerEl.empty();
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+			.setName("Setting #1")
+			.setDesc("It's a secret")
+			.addText((text) =>
+				text
+					.setPlaceholder("Enter your secret")
+					.setValue(this.plugin.settings.mySetting)
+					.onChange(async (value) => {
+						this.plugin.settings.mySetting = value;
+						await this.plugin.saveSettings();
+					})
+			);
+	}
+}
+
+class ChooseTypeModal extends Modal {
+	constructor(app: App) {
+		super(app);
+	}
+
+	onOpen(): void {
+		const { contentEl } = this;
+		console.log("whoa!");
+
+		const title = contentEl.createEl("h3", {
+			text: "请选择要创建的文件类型",
+			cls: "modal-title",
+		});
+
+		const buttonsContainer = contentEl.createDiv({
+			cls: "buttons-container",
+		});
+
+		const docButton = buttonsContainer.createEl("button", {
+			text: "创建文档",
+			cls: "modal-button",
+		});
+
+		const sheetButton = buttonsContainer.createEl("button", {
+			text: "创建表格",
+			cls: "modal-button",
+		});
+
+		docButton.onclick = () => {
+			console.log("begin to create docs");
+			this.close();
+		};
+
+		sheetButton.onclick = () => {
+			console.log("begin to create sheet");
+			this.close;
+		};
+	}
+
+	onClose() {
+		const { contentEl } = this;
+		console.log('ops~')
+		contentEl.empty();
 	}
 }
