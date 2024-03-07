bac9ecd40c346edd04766c517e44e3635fbcc814
karl <2557075675@qq.com>
2024-03-07 10:59:50 +0800

    feat: add the univer-docs demo

--- main.ts
+++ main.ts
@@ -1,13 +1,6 @@
-import {
-	App,
-	Editor,
-	MarkdownView,
-	Modal,
-	Notice,
-	Plugin,
-	PluginSettingTab,
-	Setting,
-} from "obsidian";
+// import { MyReactView } from "UniverService";
+import { App, Modal, Plugin } from "obsidian";
+import { MyReactView } from 'demo'
 
 interface MyPluginSettings {
 	mySetting: string;
@@ -28,13 +21,29 @@ export default class MyPlugin extends Plugin {
 			"cable",
 			"Univer",
 			(evt: MouseEvent) => {
-				new ChooseTypeModal(this.app).open();
+				const modal = new ChooseTypeModal(this.app);
+				modal.plugin = this;
+				modal.open();
+				this.activateView();
 			}
 		);
 		ribbonIconEl.addClass("my-plugin-ribbon-class");
 
 		const statusBarItemEl = this.addStatusBarItem();
 		statusBarItemEl.setText("Hello Univer");
+
+		this.registerView(
+			MyReactView.viewType,
+			(leaf) => new MyReactView(leaf)
+		);
+
+		this.addCommand({
+			id: "open-my-react-view",
+			name: "Open My React View",
+			callback: () => {
+				this.activateView();
+			},
+		});
 
 		this.addCommand({
 			id: "open-choose-type-modal",
@@ -43,49 +52,15 @@ export default class MyPlugin extends Plugin {
 				new ChooseTypeModal(this.app).open();
 			},
 		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: "sample-editor-command",
-			name: "Sample editor command",
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection("Sample Editor Command");
-			},
+	}
+
+	async activateView() {
+		let leaf = this.app.workspace.getLeaf(true); // 获取或创建一个leaf
+		await leaf.setViewState({
+			type: MyReactView.viewType,
+			active: true,
 		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: "open-sample-modal-complex",
-			name: "Open sample modal (complex)",
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView =
-					this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			},
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, "click", (evt: MouseEvent) => {
-			console.log("click", evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(
-			window.setInterval(() => console.log("setInterval"), 5 * 60 * 1000)
-		);
+		this.app.workspace.revealLeaf(leaf); // 显示这个leaf
 	}
 
 	onunload() {
@@ -93,8 +68,6 @@ export default class MyPlugin extends Plugin {
 	}
 
 	async loadSettings() {
-		// this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-
 		const data = await this.loadData();
 		if (data) {
 			this.settings = Object.assign({}, DEFAULT_SETTINGS, data);
@@ -109,51 +82,9 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const { contentEl } = this;
-		contentEl.setText("Woah!");
-	}
-
-	onClose() {
-		const { contentEl } = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
+class ChooseTypeModal extends Modal {
 	plugin: MyPlugin;
 
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const { containerEl } = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName("Setting #1")
-			.setDesc("It's a secret")
-			.addText((text) =>
-				text
-					.setPlaceholder("Enter your secret")
-					.setValue(this.plugin.settings.mySetting)
-					.onChange(async (value) => {
-						this.plugin.settings.mySetting = value;
-						await this.plugin.saveSettings();
-					})
-			);
-	}
-}
-
-class ChooseTypeModal extends Modal {
 	constructor(app: App) {
 		super(app);
 	}
@@ -183,6 +114,8 @@ class ChooseTypeModal extends Modal {
 
 		docButton.onclick = () => {
 			console.log("begin to create docs");
+			this.plugin.activateView();
+			// how to do
 			this.close();
 		};
 
@@ -194,7 +127,7 @@ class ChooseTypeModal extends Modal {
 
 	onClose() {
 		const { contentEl } = this;
-		console.log('ops~')
+		console.log("ops~");
 		contentEl.empty();
 	}
 }
