4d81ff8c8197b3b110acc2575735aefea951437d
karl <2557075675@qq.com>
2024-03-11 14:41:20 +0800

    feat: remove the spare file & test file

--- main.ts
+++ main.ts
@@ -5,7 +5,26 @@ import "@univerjs/sheets-ui/lib/index.css";
 import "@univerjs/sheets-formula/lib/index.css";
 
 import { App, ItemView, Modal, Plugin, WorkspaceLeaf } from "obsidian";
-import { initialUniverDocs, initialUniverSheets } from "docs";
+
+import { LocaleType, Univer, LogLevel } from "@univerjs/core";
+import { defaultTheme } from "@univerjs/design";
+import { UniverRenderEnginePlugin } from "@univerjs/engine-render";
+import { UniverFormulaEnginePlugin } from "@univerjs/engine-formula";
+import { UniverDocsUIPlugin } from "@univerjs/docs-ui";
+import { UniverUIPlugin } from "@univerjs/ui";
+import { UniverDocsPlugin } from "@univerjs/docs";
+import { UniverSheetsPlugin } from "@univerjs/sheets";
+import { UniverSheetsFormulaPlugin } from "@univerjs/sheets-formula";
+import { UniverSheetsNumfmtPlugin } from "@univerjs/sheets-numfmt";
+import { UniverSheetsUIPlugin } from "@univerjs/sheets-ui";
+import { UniverSheetsZenEditorPlugin } from "@univerjs/sheets-zen-editor";
+import {
+	UniverRPCMainThreadPlugin,
+	IUniverRPCMainThreadConfig,
+} from "@univerjs/rpc";
+import { UniverFindReplacePlugin } from "@univerjs/find-replace";
+import { DEFAULT_DOCUMENT_DATA_CN } from "./data/default-document-data-cn";
+import { DEFAULT_WORKBOOK_DATA_DEMO } from "./data/default-workbook-data-demo";
 
 interface MyPluginSettings {
 	mySetting: string;
@@ -72,7 +91,7 @@ export default class MyPlugin extends Plugin {
 	}
 
 	async activateView(type: "doc" | "sheet") {
-		let leaf = this.app.workspace.getLeaf(true);
+		const leaf = this.app.workspace.getLeaf(true);
 
 		await leaf
 			.setViewState({
@@ -117,6 +136,7 @@ class ChooseTypeModal extends Modal {
 		const { contentEl } = this;
 		console.log("whoa!");
 
+		// eslint-disable-next-line @typescript-eslint/no-unused-vars
 		const title = contentEl.createEl("h3", {
 			text: "请选择要创建的文件类型",
 			cls: "modal-title",
@@ -222,3 +242,80 @@ class UniverSheetView extends ItemView {
 // 		console.error("failed to load style.sheet");
 // 	}
 // }
+
+
+
+export async function initialUniverDocs() {
+
+	const univer = new Univer({
+		theme: defaultTheme,
+		locale: LocaleType.ZH_CN,
+	});
+
+	univer.registerPlugin(UniverRenderEnginePlugin);
+	univer.registerPlugin(UniverFormulaEnginePlugin);
+
+	univer.registerPlugin(UniverUIPlugin, {
+		container: "doc-app",
+		header: true,
+		toolbar: true,
+	});
+
+	univer.registerPlugin(UniverDocsPlugin, {
+		standalone: true,
+	});
+	univer.registerPlugin(UniverDocsUIPlugin, {
+		container: "univerdoc",
+		layout: {
+			docContainerConfig: {
+				innerLeft: false,
+			},
+		},
+	});
+
+	univer.createUniverDoc(DEFAULT_DOCUMENT_DATA_CN);
+}
+
+export function initialUniverSheets() {
+	const univer = new Univer({
+		theme: defaultTheme,
+		locale: LocaleType.ZH_CN,
+		logLevel: LogLevel.VERBOSE,
+	});
+
+	univer.registerPlugin(UniverDocsPlugin, {
+		hasScroll: false,
+	});
+	univer.registerPlugin(UniverRenderEnginePlugin);
+	univer.registerPlugin(UniverUIPlugin, {
+		container: "sheet-app",
+		header: true,
+		toolbar: true,
+		footer: true,
+	});
+
+	univer.registerPlugin(UniverDocsUIPlugin);
+
+	univer.registerPlugin(UniverSheetsPlugin, {
+		notExecuteFormula: true,
+	});
+	univer.registerPlugin(UniverSheetsUIPlugin);
+
+	// sheet feature plugins
+
+	univer.registerPlugin(UniverSheetsNumfmtPlugin);
+	univer.registerPlugin(UniverSheetsZenEditorPlugin);
+	univer.registerPlugin(UniverFormulaEnginePlugin, {
+		notExecuteFormula: true,
+	});
+	univer.registerPlugin(UniverSheetsFormulaPlugin);
+	univer.registerPlugin(UniverRPCMainThreadPlugin, {
+		workerURL: "./worker.js",
+	} as IUniverRPCMainThreadConfig);
+
+	// find replace
+	univer.registerPlugin(UniverFindReplacePlugin);
+
+	// create univer sheet instance
+	univer.createUniverSheet(DEFAULT_WORKBOOK_DATA_DEMO);
+}
