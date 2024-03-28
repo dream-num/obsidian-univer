7b6a67857ecc50acf712c4978ca70266ff5fa6e5
karl <2557075675@qq.com>
2024-03-28 18:00:19 +0800

    feat: update the sheet & doc version

--- src/views/usheet.ts
+++ src/views/usheet.ts
@@ -1,4 +1,9 @@
-import { Tools, type IWorkbookData, type Univer, type Workbook } from "@univerjs/core";
+import {
+  Tools,
+  type IWorkbookData,
+  type Univer,
+  type Workbook,
+} from "@univerjs/core";
 import type { WorkspaceLeaf } from "obsidian";
 import { TextFileView } from "obsidian";
 import { FUniver } from "@univerjs/facade";
@@ -7,22 +12,27 @@ import { sheetInit } from "~/utils/univer";
 export const Type = "univer-sheet";
 
 export class USheetView extends TextFileView {
-  contentData: string;
   rootContainer: HTMLDivElement;
   univer: Univer;
   workbook: Workbook;
   FUniver: FUniver;
+  sheetData: IWorkbookData | object;
 
   constructor(leaf: WorkspaceLeaf) {
     super(leaf);
   }
 
   getViewData(): string {
+    console.log("getViewData++++++++++", this.workbook);
     return JSON.stringify(this.workbook.save());
   }
 
   setViewData(data: string, _: boolean): void {
+    console.log("setViewData++++++++++", this.workbook);
+    this.domInit();
     this.univer?.dispose();
+    this.workbook?.dispose();
+
     this.univer = sheetInit({
       container: this.rootContainer,
       header: true,
@@ -30,19 +40,22 @@ export class USheetView extends TextFileView {
     });
     this.FUniver = FUniver.newAPI(this.univer);
     let sheetData: IWorkbookData | object;
-
     try {
       sheetData = JSON.parse(data);
     } catch (err) {
       sheetData = Tools.deepClone({});
     }
     setTimeout(() => {
+      console.log('univer--------------', this.univer)
+      console.log('sheetData--------------', sheetData)
+      console.log('workbook--------------', this.workbook)
       this.workbook = this.univer.createUniverSheet(sheetData);
+      this.getViewData();
     }, 0);
-    
-    this.FUniver.onCommandExecuted(() => {
-      this.requestSave();
-    });
+
+    // this.FUniver.onCommandExecuted(() => {
+    //   this.requestSave();
+    // });
   }
 
   getViewType() {
@@ -52,14 +65,23 @@ export class USheetView extends TextFileView {
   clear(): void {}
 
   async onOpen() {
-    this.rootContainer = this.contentEl as HTMLDivElement;
+    console.log('onOpen++++++++', this.workbook)
+
+  }
+
+  domInit() {
+    this.contentEl.empty();
+    this.rootContainer = this.contentEl.createDiv();
     this.rootContainer.id = "usheet-app";
     this.rootContainer.classList.add("uproduct-container");
   }
 
   async onClose() {
-
+    console.log("onClose++++++++++", this.univer, this.workbook);
     this.requestSave();
-    this.univer.dispose();
+    this.univer?.dispose();
+    this.workbook?.dispose();
+    this.contentEl.empty();
+    console.log("onClose-------------", this.univer, this.workbook, this.contentEl);
   }
 }
