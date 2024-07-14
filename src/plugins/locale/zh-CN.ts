d0c80f153738c50bc2ad92b2a36e50d6266e48bb
chengkai48 <chengkai48@jd.com>
2024-07-14 12:07:32 +0800

    feat:update the sheets and docs to 0.2.3

--- src/plugins/locale/zh-CN.ts
+++ src/plugins/locale/zh-CN.ts
@@ -1,5 +1,5 @@
 export default {
-  exchage: {
+  exchange: {
     uploadJson: '打开（文件）',
     downloadJson: '保存到本地',
     uploadError: '打开失败',
--- src/univer/docs.ts
+++ src/univer/docs.ts
@@ -1,27 +1,44 @@
-import { Univer } from '@univerjs/core'
+/**
+ * Copyright 2023-present DreamNum Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { Univer, UserManagerService } from '@univerjs/core'
 import { defaultTheme } from '@univerjs/design'
 import { UniverDocsPlugin } from '@univerjs/docs'
 import { UniverDocsUIPlugin } from '@univerjs/docs-ui'
-import { UniverFormulaEnginePlugin } from '@univerjs/engine-formula'
 import { UniverRenderEnginePlugin } from '@univerjs/engine-render'
 import { UniverUIPlugin } from '@univerjs/ui'
-import type { IUniverUIConfig } from '@univerjs/ui/lib/types/ui-plugin'
+import { UniverFormulaEnginePlugin } from '@univerjs/engine-formula'
+import { UniverDocsThreadCommentUIPlugin } from '@univerjs/docs-thread-comment-ui'
+import { UniverDocsHyperLinkUIPlugin } from '@univerjs/docs-hyper-link-ui'
+import type { IUniverUIConfig } from '@univerjs/ui/lib/types/controllers/ui/ui.controller'
 import { getLanguage, univerLocales } from '@/utils/common'
 import type { UniverPluginSettings } from '@/types/setting'
 
-export function docInit(
-  option: IUniverUIConfig,
-  settings: UniverPluginSettings,
-) {
+export function docInit(option: IUniverUIConfig, settings: UniverPluginSettings) {
   const univer = new Univer({
     theme: defaultTheme,
     locale: getLanguage(settings),
     locales: univerLocales,
   })
 
+  // core plugins
   univer.registerPlugin(UniverRenderEnginePlugin)
   univer.registerPlugin(UniverFormulaEnginePlugin)
   univer.registerPlugin(UniverUIPlugin, option)
+
   univer.registerPlugin(UniverDocsPlugin)
   univer.registerPlugin(UniverDocsUIPlugin, {
     container: 'univerdoc',
@@ -31,6 +48,22 @@ export function docInit(
       },
     },
   })
+
+  // univer.registerPlugin(UniverDocsDrawingUIPlugin)
+  univer.registerPlugin(UniverDocsThreadCommentUIPlugin)
+  univer.registerPlugin(UniverDocsHyperLinkUIPlugin)
+
+  const injector = univer.__getInjector()
+  const userManagerService = injector.get(UserManagerService)
+
+  const mockUser = {
+    userID: 'Owner_qxVnhPbQ',
+    name: 'Owner',
+    avatar: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAACXBIWXMAAAsTAAALEwEAmpwYAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAInSURBVHgBtZU9TxtBEIbfWRzFSIdkikhBSqRQkJqkCKTCFkqVInSUSaT0wC8w/gXxD4gU2nRJkXQWhAZowDUUWKIwEgWWbEEB3mVmx3dn4DA2nB/ppNuPeWd29mMIPXDr+RxwtgRHeW6+guNPRxogqnL7Dwz9psJ27S4NShaeZTH3kwXy6I81dlRKcmRui88swdq9AcSFL7Buz1Vmlns64MiLsCjzwnIYHLH57tbfFbs7KRaXyEU8FVZofqccOfA5l7Q8LPIkGrwnb2RPNEXWFVMUF3L+kDCk0btDDAMzOm5YfAHDwp4tG74wnzAsiOYMnJ3GoDybA7IT98/jm5+JNnfiIzAS6LlqHQBN/i6b2t/cV1Hh6BfwYlHnHP4AXi5q/8kmMMpOs8+BixZw/Fd6xUEHEbnkgclvQP2fGp7uShRKnQ3G32rkjV1th8JhIGG7tR/JyjGteSOZELwGMmNqIIigRCLRh2OZIE6BjItdd7pCW6Uhm1zzkUtungSxwEUzNpQ+GQumtH1ej1MqgmNT6vwmhCq5yuwq56EYTbgeQUz3yvrpV1b4ok3nYJ+eYhgYmjRUqErx2EDq0Fr8FhG++iqVGqxlUJI/70Ar0UgJaWHj6hYVHJrfKssAHot1JfqwE9WVWzXZVd5z2Ws/4PnmtEjkXeKJDvxUecLbWOXH/DP6QQ4J72NS0adedp1aseBfXP8odlZFfPvBF7SN/8hky1TYuPOAXAEipMx15u5ToAAAAABJRU5ErkJggg==',
+    anonymous: false,
+    canBindAnonymous: false,
+  }
+  userManagerService.setCurrentUser(mockUser)
 
   return univer
 }
