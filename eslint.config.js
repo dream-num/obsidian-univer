0184549ccf71269af8963cd5b385932940cfedc5
Anna cheng <annacheng@192.168.1.145>
2024-09-07 11:30:05 +0800

    fix:remove the eslint of issue template

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,7 @@
 import antfu from '@antfu/eslint-config'
 
 export default antfu({
-  ignores: ['**/data/**.ts'],
+  ignores: ['**/data/**.ts', '.github/ISSUE_TEMPLATE/**'],
 }, {
   rules: {
     'no-new': 'off',
